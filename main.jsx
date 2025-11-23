917a8023f0db5e1af474c00b7f6d778f8e6e2839
balajimusicworld8-coder <balajimusicworld8@gmail.com>
2025-11-23 12:42:05 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,7 @@
-import React from 'react'\nimport { createRoot } from 'react-dom/client'\nimport App from './App.jsx'\nimport './index.css'\n\ncreateRoot(document.getElementById('root')).render(<App />)\n
+
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(<App />)
